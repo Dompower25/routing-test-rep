8a07e7194e70a161723bd5b6cb24b8299cd518c6
Eugene Domanovich <89357747+Dompower25@users.noreply.github.com>
2025-08-25 15:42:19 +0000

    create store

---
+++ src/Counter.jsx
@@ -0,0 +1,19 @@
+import React, { FC } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./redux/slices/counterSlice";
+
+
+const Counter = () => {
+    const count = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch();
+
+    return (
+        <div>
+            <h2>{count}</h2>
+            <button onClick={() => {dispatch(increment())}}>inctrement</button>
+            <button onClick={() => {dispatch(decrement())}}>decrement</button>
+        </div>
+    )
+}
+
+export default Counter
